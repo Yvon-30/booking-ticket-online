f4d9d0393cadc30fc7bc1462c5115d72a4463828
Yvon MENSAH <yvonmensah22@gmail.com>
2024-10-01 15:09:30 +0100

    Amélioration des cards pour le carousel

--- js/script.js
+++ js/script.js
@@ -49,3 +49,25 @@ document.getElementById('confirmBooking').addEventListener('click', function() {
     modal.hide();
     alert('Votre réservation a été effectuée avec succès.');
 });
+
+const movieModal = document.getElementById('movieModal');
+        movieModal.addEventListener('show.bs.modal', function (event) {
+            const button = event.relatedTarget;
+            const title = button.getAttribute('data-title');
+            const description = button.getAttribute('data-description');
+            const actors = button.getAttribute('data-actors');
+            const image = button.getAttribute('data-image');
+            const link = button.getAttribute('data-link');
+
+            const modalTitle = movieModal.querySelector('.modal-title');
+            const modalDescription = movieModal.querySelector('#movieDescription');
+            const modalActors = movieModal.querySelector('#movieActors');
+            const modalImage = movieModal.querySelector('#movieImage');
+            const modalLink = movieModal.querySelector('#moreDetailsLink');
+
+            modalTitle.textContent = title;
+            modalDescription.textContent = description;
+            modalActors.textContent = actors;
+            modalImage.src = image;
+            modalLink.href = link;
+        });
